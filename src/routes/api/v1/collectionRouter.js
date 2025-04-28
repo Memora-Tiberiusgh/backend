cba5df2647a4418d482c9b2c56f1d1e36e44543e
Tiberius Gherac <tg222hh@student.lnu.se>
2025-04-28 10:40:47 +0200

    Update name of parameter

--- src/routes/api/v1/collectionRouter.js
+++ src/routes/api/v1/collectionRouter.js
@@ -12,10 +12,25 @@ export const router = express.Router()
 
 const controller = new CollectionController()
 
+// Provide req.collection to the route if :id is present in the route path.
+router.param("collectionId", (req, res, next, collectionId) =>
+  controller.loadCollectionDocument(req, res, next, collectionId)
+)
+
 router.get("/", async (req, res, next) =>
   controller.collectionsGet(req, res, next)
 )
 
 router.post("/", async (req, res, next) =>
   controller.createCollection(req, res, next)
+)
+
+router.get("/:collectionId", async (req, res, next) =>
+  controller.loadCollectionDocument(req, res, next)
+)
+router.patch("/:collectionId", async (req, res, next) =>
+  controller.updateCollection(req, res, next)
+)
+router.delete("/:collectionId", async (req, res, next) =>
+  controller.deleteCollection(req, res, next)
 )
